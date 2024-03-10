5692ada2d98e8eb9fce0e781a10362002f85fd04
Vladislav Dankevich <vielendanke1991@gmail.com>
2024-03-10 19:27:13 +0500

    code structure

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-pub mod collections;
-pub mod exceptions;
+pub mod learning;
 
 fn main() {
-    exceptions::recoverable_result::file_not_exist_recoverable(&"hello.txt".to_string())
+    learning::exceptions::recoverable_result::file_not_exist_recoverable(&"hello.txt".to_string())
 }
