dd86f0e891be65a87ac4ed25d3944d27a5c8ab3e
Vladislav Dankevich <vielendanke1991@gmail.com>
2024-04-06 14:15:35 +0500

    Learning continue

--- src/traits/return_traits.rs
+++ src/traits/return_traits.rs
@@ -1,30 +1,30 @@
-// it won't work because we need to return either Tween or NewsArticle
-// pub fn returns_summarizable(switch: bool) -> impl Summary {
-//     if switch {
-//         NewsArticle {
-//             headline: String::from(
-//                 "Penguins win the Stanley Cup Championship!",
-//             ),
-//             location: String::from("Pittsburgh, PA, USA"),
-//             author: String::from("Iceburgh"),
-//             content: String::from(
-//                 "The Pittsburgh Penguins once again are the best \
-//                  hockey team in the NHL.",
-//             ),
-//         }
-//     } else {
-//         Tweet {
-//             username: String::from("horse_ebooks"),
-//             content: String::from(
-//                 "of course, as you probably already know, people",
-//             ),
-//             reply: false,
-//             retweet: false,
-//         }
-//     }
-// }
+// dynamic return of any value implemented trait Summary
+pub fn returns_summarizable_dynamic(switch: bool) -> Box<dyn Summary> {
+    if switch {
+        Box::new(NewsArticle {
+            headline: String::from(
+                "Penguins win the Stanley Cup Championship!",
+            ),
+            location: String::from("Pittsburgh, PA, USA"),
+            author: String::from("Iceburgh"),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best \
+                 hockey team in the NHL.",
+            ),
+        })
+    } else {
+        Box::new(Tweet {
+            username: String::from("horse_ebooks"),
+            content: String::from(
+                "of course, as you probably already know, people",
+            ),
+            reply: false,
+            retweet: false,
+        })
+    }
+}
 
-use crate::traits::create_and_implement_trait::{Summary, Tweet};
+use crate::traits::create_and_implement_trait::{NewsArticle, Summary, Tweet};
 
 pub fn returns_summarizable() -> impl Summary {
     Tweet {
