989d59a4a7aeaacd17d460406cc6ee7cfe2d4d05
Vladislav Dankevich <vielendanke1991@gmail.com>
2024-05-04 17:36:53 +0500

    more tasks

--- learning/src/closures.rs
+++ learning/src/closures.rs
@@ -4,5 +4,5 @@ pub mod links_and_ownership;
 
 // 3 types of closures
 // FnOnce() -> T - the function is called only once, returns a value from it's body, do not accept any values
-// FnMut(V) -> T - could be called more than once, could modify state, do not return anything from it's body
-// Fn(V) -> T - could be called more than once, couldn't modify state, could be used without any capture at all
+// FnMut(V) -> T - could be called more than once, could modify state
+// Fn(V) -> T - could be called more than once, couldn't modify state
